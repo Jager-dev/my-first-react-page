2499b892caa10586ffc2c201d0611619a28b1cb0
Ulan Zhakypov <zhakypovu@gmail.com>
2021-08-17 13:57:51 +0300

    header commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+import Nav from "./Nav";
+import './header.css';
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Logo />
+      <Nav />
+    </header>
+  )
+}
+export default Header
---
+++ src/Nav.js
@@ -0,0 +1,24 @@
+import './nav.css'
+
+const Nav = () => {
+  return (
+    <ul className="nav-list">
+      <li className="nav-list-item">
+        <a href="#">Доставка</a>
+      </li>
+      <li className="nav-list-item">
+        <a href="#">Оплата</a>
+      </li>
+      <li className="nav-list-item">
+        <a href="#">Отзывы</a>
+      </li>
+      <li className="nav-list-item">
+        <a href="#">Вопрос - ответ</a>
+      </li>
+      <li className="nav-list-item">
+        <a href="#">Контакты</a>
+      </li>
+    </ul>
+  )
+}
+export default Nav
